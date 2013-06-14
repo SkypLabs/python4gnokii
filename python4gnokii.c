34d696a9cf7dd3cf6f82941feb5cf434353478f8
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2013-06-14 15:15:46 +0200

    add 'answercall()'

--- python4gnokii.c
+++ python4gnokii.c
@@ -23,7 +23,7 @@ static PyObject *gnokii_open(PyObject *self, PyObject *args)
 	
 	if (GN_ERR_NONE != error)
 	{
-		PyErr_SetString(GnokiiError, "Open command failed");
+		PyErr_SetString(GnokiiError, "[x] Open command failed");
 		return NULL;
 	}
 
@@ -39,6 +39,8 @@ static PyObject *gnokii_close(PyObject *self, PyObject *args)
 	gn_lib_phoneprofile_free(&state);
 	gn_lib_library_free();
 
+	connected = 0;
+
 	Py_RETURN_NONE;
 }
 
@@ -53,7 +55,7 @@ static PyObject *gnokii_dialvoice(PyObject *self, PyObject *args)
 
 	if (!connected)
 	{
-		PyErr_SetString(GnokiiError, "Not connected !");
+		PyErr_SetString(GnokiiError, "[x] Not connected !");
 		return NULL;
 	}
 
@@ -70,11 +72,47 @@ static PyObject *gnokii_dialvoice(PyObject *self, PyObject *args)
 
 	if ((error = gn_call_dial(&call_id, data, state)) != GN_ERR_NONE)
 	{
-		PyErr_SetString(GnokiiError, "Dialvoice failed");
+		PyErr_SetString(GnokiiError, "[x] Dialvoice failed");
 		return NULL;
 	}
 
 	return Py_BuildValue("i", call_info.call_id);
+}
+
+static PyObject *gnokii_answercall(PyObject *self, PyObject *args)
+{
+	const char *callid;
+    	gn_call_info callinfo;
+	gn_error error;
+
+	if (!connected)
+	{
+		PyErr_SetString(GnokiiError, "[x] Not connected !");
+		return NULL;
+	}
+
+	if (!PyArg_ParseTuple(args, "b", &callid))
+		return NULL;
+
+	memset(&callinfo, 0, sizeof(callinfo));
+	callinfo.call_id = strtol(callid, NULL, 10);
+
+	if (callinfo.call_id < 0)
+	{
+		PyErr_SetString(GnokiiError, "[x] Invalid call id");
+		return NULL;
+	}
+
+	gn_data_clear(data);
+	data->call_info = &callinfo;
+
+	error = gn_sm_functions(GN_OP_AnswerCall, data, state);
+
+	if (error != GN_ERR_NONE)
+	{
+		PyErr_SetString(GnokiiError, "[x] Answercall failed");
+		return NULL;
+	}
 
 	Py_RETURN_NONE;
 }
@@ -83,6 +121,7 @@ static PyMethodDef GnokiiMethods[] = {
 	{"open", gnokii_open, METH_VARARGS, "Initiate connection to phone."},
 	{"close", gnokii_close, METH_VARARGS, "Close connection to phone."},
 	{"dialvoice", gnokii_dialvoice, METH_VARARGS, "Initiate voice call."},
+	{"answercall", gnokii_answercall, METH_VARARGS, "Answer an incoming call."},
 	{NULL, NULL, 0, NULL}
 };
 

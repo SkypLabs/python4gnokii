5b74b1bc505adbd6a78a53d7621aafb1d867c6d8
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2016-11-24 17:52:14 +0000

    update package structure

---
+++ README.rst
@@ -0,0 +1,52 @@
+=============
+python4gnokii
+=============
+
+python4gnokii is a Python bindings for the
+`Gnokii <http://gnokii.org/>`__ project.
+
+Dependencies
+============
+
+To build and make the Python module work, you need the following
+elements :
+
+-  **libgnokii** and its headers
+-  The **`setuptools <https://pypi.python.org/pypi/setuptools>`__**
+   package
+
+On Fedora
+---------
+
+::
+
+    yum install gnokii gnokii-devel python-setuptools
+
+On Debian
+---------
+
+::
+
+    aptitude install gnokii-cli libgnokii-dev python-setuptools
+
+Installation
+============
+
+::
+
+    git clone git@github.com:SkypLabs/python4gnokii.git
+    cd python4gnokii
+    python setup.py install
+
+How to
+======
+
+The best way to learn how it works is to look at the examples available
+in the
+`examples <https://github.com/SkypLabs/python4gnokii/tree/master/examples>`__
+folder.
+
+License
+=======
+
+`GPL version 3 <https://www.gnu.org/licenses/gpl.txt>`__
--- setup.py
+++ setup.py
@@ -1,18 +1,46 @@
-from distutils.core import setup, Extension
+#!/usr/bin/env python3
+
+from setuptools import setup, Extension
+from os.path import dirname, abspath, join
+from codecs import open
+
+DIR = dirname(abspath(__file__))
+VERSION = '0.2.0'
  
 gnokii = Extension(
-	'gnokii',
-	libraries = ['gnokii'],
-	library_dirs = ['/usr/local/lib/'],
-	sources = ['python4gnokii.c']
+    'gnokii',
+    libraries = ['gnokii'],
+    library_dirs = ['/usr/local/lib/'],
+    sources = [
+        DIR + '/src/python4gnokii.c',
+    ],
 )
+
+with open(join(DIR, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
  
 setup(
-	name = 'python4gnokii',
-	version = '0.1.0',
-	description = 'Python bindings for the Gnokii library',
-	author = 'Paul-Emmanuel Raoul',
-	author_email = 'skyper@skyplabs.net',
-	url = 'https://github.com/SkypLabs/python4gnokii',
-	ext_modules = [gnokii],
+    name = 'python4gnokii',
+    version = VERSION,
+    description = 'Python bindings for the Gnokii library',
+    long_description = long_description,
+    license = 'GPLv3',
+    keywords = 'gnokii bindings',
+    author = 'Paul-Emmanuel Raoul',
+    author_email = 'skyper@skyplabs.net',
+    url = 'https://github.com/SkypLabs/python4gnokii',
+    download_url = 'https://github.com/SkypLabs/python4gnokii/archive/v{0}.zip'.format(VERSION),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    ext_modules = [gnokii],
 )

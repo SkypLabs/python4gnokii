6d806d53e5aa404c3f14f813472af9ada39ab5de
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2016-12-06 01:33:42 +0000

    replace tab characters with spaces in the Python example

--- examples/sqlite_sms.py
+++ examples/sqlite_sms.py
@@ -8,33 +8,33 @@ conn = connect('sms.db')
 
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS sms (
-	id INTEGER PRIMARY KEY NOT NULL,
-	time TIMESTAMP,
-	num TEXT,
-	msg TEXT);'''
+    id INTEGER PRIMARY KEY NOT NULL,
+    time TIMESTAMP,
+    num TEXT,
+    msg TEXT);'''
 )
 
 print('[*] Start SMS reception ...')
 
 try:
-	while True:
-		messages = getsms('SM', 1, 5)
-		i=0
+    while True:
+        messages = getsms('SM', 1, 5)
+        i=0
 
-		for msg in messages:
-			i += 1
+        for msg in messages:
+            i += 1
 
-			if msg:
-				print("From : {0}".format(msg[2]))
-				print("Message : {0}".format(msg[4]))
-				print("----------")
+            if msg:
+                print("From : {0}".format(msg[2]))
+                print("Message : {0}".format(msg[4]))
+                print("----------")
 
-				c.execute("INSERT INTO sms(time, num, msg) VALUES (DateTime('now'), '{0}', '{1}');".format(msg[2], msg[4]))
-				conn.commit()
+                c.execute("INSERT INTO sms(time, num, msg) VALUES (DateTime('now'), '{0}', '{1}');".format(msg[2], msg[4]))
+                conn.commit()
 
-				deletesms('SM', i)
+                deletesms('SM', i)
 except KeyboardInterrupt as e:
-	conn.commit()
-	conn.close()
-	print('[*] Bye !')
-	exit(0)
+    conn.commit()
+    conn.close()
+    print('[*] Bye !')
+    exit(0)

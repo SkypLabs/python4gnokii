391a836c5f506463ad1b3b25000989c05b04d87a
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2018-04-27 16:27:02 +0100

    update 'setup.py'

--- setup.py
+++ setup.py
@@ -2,21 +2,25 @@
 
 from setuptools import setup, Extension
 from os.path import dirname, abspath, join
-from codecs import open
+from codecs import open as fopen
 
 DIR = dirname(abspath(__file__))
 VERSION = '0.3.0'
 
 gnokii = Extension(
     'gnokii',
-    libraries = ['gnokii'],
-    library_dirs = ['/usr/local/lib/'],
     sources = [
         DIR + '/src/python4gnokii.c',
     ],
+    libraries = [
+        'gnokii',
+    ],
+    library_dirs = [
+        '/usr/local/lib/',
+    ],
 )
 
-with open(join(DIR, 'README.rst'), encoding='utf-8') as f:
+with fopen(join(DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -25,7 +29,7 @@ setup(
     description = 'Python bindings for the Gnokii library',
     long_description = long_description,
     license = 'GPLv3',
-    keywords = 'gnokii bindings',
+    keywords = 'gnokii bindings gsm sms',
     author = 'Paul-Emmanuel Raoul',
     author_email = 'skyper@skyplabs.net',
     url = 'https://github.com/SkypLabs/python4gnokii',
@@ -40,7 +44,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
     ext_modules = [gnokii],
+    python_requires='>=3',
 )
